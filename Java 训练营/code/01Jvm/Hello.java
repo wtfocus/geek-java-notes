488f13e0d2a0f1b758d3ff8be3f30b87598fe971
wangtao03 <wangtao03@playcrab.com>
2021-11-08 10:19:44 +0800

    up Java 训练营 01Jvm

---
+++ Java 训练营/code/01Jvm/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+    public static void main(String[] args) {
+        byte n1 = 1;
+        int n2 = 2;
+        long n3 = 3L;
+        double n4 = 4.0D;
+
+        if (n2 < n3) {
+            System.out.println("n2 + n3 = " + n2 + n3);
+        }
+
+        for (double i = 0; i <= n4; i++) {
+            System.out.println("n1 + i = " + n1 + i);
+        }
+    }
+}
