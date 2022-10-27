9f4b4f93267a6e4593e7bc17f9fc154e4bd05720
wangtao <wangtao03@playcrab.com>
2022-10-27 14:54:25 +0800

    update java 业务开发 100 例

---
+++ Java 业务开发 100 例/code/java-100-mistakes/src/main/java/org/example/n01/concurrenthashmap/APP.java
@@ -0,0 +1,13 @@
+package org.example.n01.concurrenthashmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APP {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(APP.class, args);
+    }
+}
